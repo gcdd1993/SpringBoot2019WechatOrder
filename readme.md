1466c30887146a3d3ad2ee23fe8a81fb82fdcd9f
gaochen <gaochen@maxtropy.com>
2019-09-08 11:09:44 +0800

    commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# 文档
+1. 课程中的文档都在doc目录里
+
+# 支付相关
+1. 授权和支付分开调试，授权用微信官方测试账号，支付用借我的账号，写死openid调试
+2. 微信授权 https://www.imooc.com/article/70497
+1. 微信支付 https://www.imooc.com/article/31607
+1. 扫码登录调试文档 见doc目录下的open.md
+
+
+# 代码更新记录
+#### 2018-6-5
+1. 后台图片上传使用又拍云，[又拍云免费体验](https://console.upyun.com/register/?invite=HyTufSjS-)# SpringBoot2019WechatOrder
---
+++ src/main/java/com/imooc/SellApplication.java
@@ -0,0 +1,16 @@
+package com.imooc;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.imooc.dataobject.mapper")
+@EnableCaching
+public class SellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellApplication.class, args);
+	}
+}
